f527570297c99c8d973b5fdffbbd00acba47ce1e
russel2005 <russel2005@gmail.com>
2021-03-03 01:09:18 -0500

    post api added

---
+++ API/POST/post_create_user.py
@@ -0,0 +1,15 @@
+import requests
+
+payload={
+    "name": "russel",
+    "job": "SDET"
+}
+
+resp = requests.post("https://reqres.in/api/users", data=payload)
+
+code = resp.status_code
+assert code == 201, "Code doesn't match"
+
+assert resp.json()['job'] == 'SDET', 'job does not match'
+
+
